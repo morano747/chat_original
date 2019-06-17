cbeef73d1b61cfa926044fe30f85bee8e2acde67
Dima <morano747@gmail.com>
2019-06-17 14:41:08 +0300

    server changes

--- server.py
+++ server.py
@@ -6,6 +6,7 @@ class Client(Protocol):
     ip: str = None
     login: str = None
     factory: 'Chat'
+    names = []
 
     def __init__(self, factory):
         """
@@ -20,7 +21,6 @@ class Client(Protocol):
         """
         self.ip = self.transport.getHost().host
         self.factory.clients.append(self)
-
         print(f"Client connected: {self.ip}")
 
         self.transport.write("Welcome to the chat v0.1\n".encode())
@@ -33,20 +33,39 @@ class Client(Protocol):
         message = data.decode().replace('\n', '')
 
         if self.login is not None:
+            #print(self.login + "1") ###
             server_message = f"{self.login}: {message}"
+            #print(server_message + "2")
+            #print(message + "3")
             self.factory.notify_all_users(server_message)
+           # print(self.factory.notify_all_users + "4")
+           # print(self.factory.notify_all_users(server_message) + "5")
+            self.factory.message.append(server_message)
+           # print(self.factory.message + "6")
+           # print(self.factory.message.append(server_message)+ "7")
+
 
             print(server_message)
-        else:
-            if message.startswith("login:"):
-                self.login = message.replace("login:", "")
-
+        else: # так как без имени клиент не может писать в чат
+            self.login = message.replace("login:", "")
+            self.factory.clients_login.append(self.login)
+            if 2 == self.factory.clients_login.count(self.login): # мы проверяем зарезервивано имя или нет
+                self.transport.write(f"login: '{self.login}' is reserved\n".encode())
+                self.transport.write(f"Your login >>> ".encode()) #усли да, то повторно просим ввести логин
+                self.factory.clients_login.remove(self.login)
+                self.login = None # и обнуляем логин, чтобы следующий ответ был присвоен как имя
+            else: #если нет, то добро пожаловать
                 notification = f"New user connected: {self.login}"
+                self.factory.message.append(notification)  # добавляет в список пользователей
+                self.factory.notify_all_users(notification, self)
 
-                self.factory.notify_all_users(notification)
+                self.transport.write("Welcome to the chat v0.1".encode())
+
+                self.transport.write("old messages:\n".encode())
+                for send in range(0, len(self.factory.message) -1 ):
+                    self.transport.write(f"{send +1}: {self.factory.message[send]}\n".encode())
                 print(notification)
-            else:
-                print("Error: Invalid client login")
+
 
     def connectionLost(self, reason=None):
         """
@@ -55,6 +74,7 @@ class Client(Protocol):
         """
         self.factory.clients.remove(self)
         print(f"Client disconnected: {self.ip}")
+
 
 
 class Chat(Factory):
